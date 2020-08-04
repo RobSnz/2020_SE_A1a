6c191b1f9ed42e4712c76d1be2e83482260b56f2
Rob Smit <54334077+RobSnz@users.noreply.github.com>
2020-08-04 17:08:46 +1200

    Renamed project

--- 2020_SE_A1a/src/tweet.js
+++ 2020_SE_A1a/src/tweet.js
@@ -5,7 +5,7 @@ function Tweet({name, message}) {
     return(
         <div className="tweet">
             <h3>{name}</h3>
-    <       p>{message}</p>
+            <p>{message}</p>
             <h3>Number of likes</h3>
         </div>
     );
