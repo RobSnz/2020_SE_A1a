7c442c56198683f73c80821a012e8cc82c2356ae
RobSnz <robert.smitnz@gmail.com>
2020-08-05 19:53:15 +1200

    Test

---
+++ 2020_SE_A1a/src/App.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import Tweet from "./tweet"
+
+function App() {
+  const [user, setUsers] = useState([
+    {name: 'Ed', message: 'Hi'},
+    {name: 'John', message: 'Hello'},
+    {name: 'Rob', message: 'Hey'}
+  ])
+
+  return(
+    <div className="app">
+      {user.map(user =>(
+        <Tweet name={user.name} message={user.message}/>
+      ))}
+    </div>
+  );
+}
+
+export default App;
