eaf5a98640c2b152b59094b1ec92b2dcc743e3c9
RobSnz <robert.smitnz@gmail.com>
2020-08-20 16:20:26 +1200

    change

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -10,7 +10,7 @@ export default class Home extends Component {
       <div className="Home">
         <div className="lander">
           <h1>Home page</h1>
-          <p>A simple app showing react button click navigation automatic change?</p>
+          <p>A simple app showing react button click navigation automatic change? again</p>
           <form>
             <Button variant="btn btn-success" onClick={() => history.push('/Products')}>Click button to view products</Button>
           </form>
