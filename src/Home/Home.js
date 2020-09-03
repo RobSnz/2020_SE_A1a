cc7bc607ccc6072023c8cd85a13a888a3f4e5623
RobSnz <robert.smitnz@gmail.com>
2020-09-03 12:56:30 +1200

    Added search bar and date pickers

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,19 +2,90 @@ import React, { Component } from "react";
 import { Button } from 'react-bootstrap';
 import history from './../history';
 import "./Home.css";
+import Calendar from 'react-calendar';
+import DatePicker from 'react-datepicker';
+import { RiArrowDropDownLine } from 'react-icons/ri';
+import "react-datepicker/dist/react-datepicker.css";
 
-export default class Home extends Component {
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state={ 
+      search: '', 
+      dateFrom: new Date(),
+      dateTo: new Date(),
+      fromDatePickerOpen: false,
+      toDatePickerOpen: false,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleFromDate = this.handleFromDate.bind(this);
+    this.handleToDate = this.handleToDate.bind(this);
+    this.openFromDatePicker = this.openFromDatePicker.bind(this);
+    this.openToDatePicker = this.openToDatePicker.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const { search } = this.state;
+
+    console.log(search);
+
+    this.setState({ search: '' })
+  }
+
+  handleSearch(event) {
+    this.setState({ search: event.target.value });
+  }
+
+  handleFromDate = dateFrom => {
+    this.setState({ dateFrom });
+  };
+
+  handleToDate = dateTo => {
+    this.setState({ dateTo })
+  };
+
+  openFromDatePicker() {
+    this.setState({ fromDatePickerOpen: !this.state.fromDatePickerOpen, toDatePickerOpen: false });
+  }
+
+  openToDatePicker() {
+    this.setState({ toDatePickerOpen: !this.state.toDatePickerOpen, fromDatePickerOpen: false });
+  }
+
   render() {
     return (
       <div className="Home">
         <div className="lander">
           <h1>SEARCH ARTICLES</h1>
-          <p>A simple app showing react button click navigation automatic change? again</p>
-          <form>
-            <Button variant="btn btn-success" onClick={() => history.push('/Products')}>Click button to view products</Button>
-          </form>
+          <form onSubmit={ this.handleSubmit }>
+            <div className="input-box">
+              <ul>
+                <input onKeyDown={ (e) => { if(e.keyCode === 13) this.handleSubmit(e);}}
+                  type='text' placeholder='What are you looking for?' onChange={ this.handleSearch }
+                  value={ this.state.email } style={{height: "40px", paddingBottom: "6px"}}
+                />
+                <button onClick={this.openFromDatePicker} style={{fontSize: "20px", height:"40px"}}>
+                  DATE FROM
+                  <DatePicker onChange={ this.handleFromDate } selected={ this.state.dateFrom } open={ this.state.fromDatePickerOpen } className="datepicker"/>
+                  <RiArrowDropDownLine/>
+                </button>
+                <button onClick={this.openToDatePicker} style={{fontSize: "20px", height:"40px"}}>
+                  DATE TO
+                  <DatePicker onChange={ this.handleToDate } selected={ this.state.dateTo } open={ this.state.toDatePickerOpen } className="datepicker"/>
+                  <RiArrowDropDownLine/>
+                </button>
+              </ul>
+            </div>
+        </form>
         </div>
       </div>
     );
   }
 }
+
+export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ import { withRouter } from 'react-router-dom';
 const Navigation = (props) => {
     console.log(props);
     return (
-        <Navbar bg="light" variant="light" fontSize="100px">
+        <Navbar bg="light" variant="light">
             <Navbar.Brand href="/">SEER</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto" font-size="100px !important">
+                <Nav className="ml-auto">
                     {/*<Nav.Link href="/"></Nav.Link>*/}
                     {/*<Nav.Link href="/About">About</Nav.Link>*/}
                     {/*<Nav.Link href="/Contact">Contact</Nav.Link>*/}
