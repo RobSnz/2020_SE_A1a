b384788c192532163ef529cd78f76886ffeca2b0
Taylor Styles <taylor@aap.co.nz>
2020-09-29 14:02:00 +1300

    got some stuff working
    love from taylor

--- backend/models/article.model.js
+++ backend/models/article.model.js
@@ -3,7 +3,17 @@ const Schema = mongoose.Schema;
 
 const articleScheme = new Schema({
     title: { type: String, required: true},
-    year: { type: String, required: true}
+    year: { type: String, required: false},
+    author: { type: String, required: false},
+    month: { type: String, required: false},
+    volume: { type: String, required: false},
+    pageNum: { type: String, required: false},
+    numOfPages: { type: String, required: false},
+    ePrint: { type: String, required: false},
+    ePrintType: { type: String, required: false},
+    ePrintClass: { type: String, required: false},
+    annote: { type: String, required: false}
+
 }, {
     timestamps: true,
 });
--- backend/routes/article.js
+++ backend/routes/article.js
@@ -8,11 +8,21 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
-    const newArticle = new Article({
-        title: "hello",
-        year: "chicken"
-    });
+    console.log(req.body)
     
+    const title = req.body.title;
+    const year = req.body.year;
+    const author = req.body.author;
+    const month = req.body.month;
+    const volume = req.body.volume;
+    const pagesNum = req.body.pagesNum;
+    const numOfPages = req.body.numOfPages;
+    const ePrint = req.body.ePrint;
+    const ePrintType = req.body.ePrintType;
+    const ePrintClass = req.body.ePrintClass;
+    const annote = req.body.annote;
+    const newArticle = new Article({title, year, author, month, volume, pagesNum, numOfPages, ePrint, ePrintType, ePrintClass, annote});  
+
     newArticle.save()
     .then(() => res.json('article added!'))
     .catch(err => res.status(400).json('err: ' + err));
