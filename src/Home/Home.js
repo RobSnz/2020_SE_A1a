5da208419f247a7c7236d7453d0c962878dc713c
Taylor Styles <taylor@aap.co.nz>
2020-10-14 23:15:47 +1300

    Began css miagration

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Home.css";
+import '../mystyle.module.css';
+
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/AddCircle';
 import RemoveIcon from '@material-ui/icons/RemoveCircle'
