8eee275554f70d8dfa1d08e5c8186d555e98a5f3
Jacob Garbarek <xnf3679@autuni.ac.nz>
2020-10-14 18:37:42 +1300

    Analyst list and pop up modal created

--- src/Analyst/Analyst.js
+++ src/Analyst/Analyst.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Home/Home.css";
+import styles from '../mystyle.module.css';
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/AddCircle';
 import RemoveIcon from '@material-ui/icons/RemoveCircle'
@@ -10,42 +10,61 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import DatePicker from "react-datepicker";
 import ListGroup from 'react-bootstrap/ListGroup';
+import Modal from 'react-bootstrap/Modal'
 //import AnalystFunctions from './AnalystFunctions.js'
 import "react-datepicker/dist/react-datepicker.css";
 //const AnalystFunctions = require('./AnalystFunctions');
 import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
 
-class Analyst extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.openArticle = this.openArticle.bind(this);
-  }
-
-  openArticle() {
-    alert('You want to open an article');
-  }
-
-  render() {
-    return (
-      <div className="Home">
-        <div className="lander">
-          <h1>ANALYSE ARTICLES</h1>
-            <ListGroup>
-              <ListGroup.Item action onClick={this.openArticle}>
-                Link 1
-              </ListGroup.Item>
-              <ListGroup.Item action onClick={this.openArticle}>
-                Link 2
-              </ListGroup.Item>
-              <ListGroup.Item action onClick={this.openArticle}>
-                This one is a button
-              </ListGroup.Item>
-            </ListGroup>
-        </div>
-      </div>
-    );
-  }
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Analysis
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Article Heading</h4>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Accept</Button>
+        <Button onClick={props.onHide}>Reject</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
+
+const Analyst = () => {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <div className={styles.analystQueue}>
+      <h1>ANALYSE ARTICLES</h1>
+      <ListGroup>
+        <ListGroup.Item action onClick={() => setModalShow(true)}>
+          Article 1
+              </ListGroup.Item>
+        <ListGroup.Item action onClick={() => setModalShow(true)}>
+          Article 2
+              </ListGroup.Item>
+        <ListGroup.Item action onClick={() => setModalShow(true)}>
+          Article 3
+              </ListGroup.Item>
+      </ListGroup>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </div>
+  );
+};
 
 export default Analyst;
