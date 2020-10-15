a65d1aadf0a422721e9b33581bab12da91344b20
Jacob Garbarek <xnf3679@autuni.ac.nz>
2020-10-15 23:23:15 +1300

    Moderator UI changed to match Analyst

--- src/Moderate/Moderate.js
+++ src/Moderate/Moderate.js
@@ -1,15 +1,29 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { Navbar } from 'react-bootstrap';
-import axios from "axios";
 import styles from '../mystyle.module.css';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/AddCircle';
+import RemoveIcon from '@material-ui/icons/RemoveCircle'
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Modal from 'react-bootstrap/Modal'
+import "react-datepicker/dist/react-datepicker.css";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import axios from "axios";
 
 class Moderate extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state={ articleList: [] }
+    this.state = {
+      show: false,
+      articleList: [],
+      modelInformation: []
+    }
   }
 
   componentDidMount = () => {
@@ -17,11 +31,12 @@ class Moderate extends React.Component {
     this.getArticleResult();
   };
 
-  handleSubmit(event, title, status) {
-    event.preventDefault();
+  handleSubmit(e, status) {
+    e.preventDefault();
+    this.componentDidMount();
 
     const body = {
-      title: title,
+      title: this.state.modelInformation.title,
       status: status
     };
 
@@ -30,11 +45,29 @@ class Moderate extends React.Component {
         console.log(response);
         alert("Successfully Updated");
         this.componentDidMount();
+        this.setShow(false);
       })
       .catch(() => {
         alert('Error retrieving data');
-    });
+      });
   }
+
+  setShow = (boolean) => {
+    this.setState({ show: boolean });
+  }
+
+  handleClose = () => {
+    this.setShow(false);
+  }
+
+  handleOpen = (e, article) => {
+    console.log(article);
+    this.setShow(true);
+  }
+
+  componentDidMount = () => {
+    this.getArticleResult();
+  };
 
   getArticleResult = () => {
     axios.post('/article/retrieve/toModerate')
@@ -47,34 +80,82 @@ class Moderate extends React.Component {
       });
   }
 
+  updateDetails = (article) => {
+    this.setState({ modelInformation: { title: article.title, 
+    author: article.author, volume: article.volume, noOfPages: article.noOfPages,
+    year: article.year, month: article.month, ePrint: article.ePrint, ePrintType: article.ePrintType,
+    ePrintClass: article.ePrintType, annote: article.annote}});
+}
+
   render() {
     return (
-      <div>
-        <Navbar bg="light" variant="light"></Navbar>
-        <div>
-          <ul className={styles.moderateStyle}>
+      <div className={styles.analystQueue}>
+        <h1>MODERATE ARTICLES</h1>
+
+        <ListGroup>
           {this.state.articleList.map(article => {
-            return <div>
-              <Card>
-                <Card.Body>
-                  <Card.Title>Title: {article.title}</Card.Title>
-                    <Card.Text>
-                      Author: {article.author}<br/>
-                      Year: {article.year}<br/>
-                      Month: {article.month}<br/>
-                      Volume: {article.volume}<br/>
-                      Number of pages: {article.numOfPages}<br/>
-                      ePrint: {article.ePrint}<br/>
-                      ePrintType: {article.ePrintType}<br/>
-                      ePrintClass: {article.annote}<br/>
-                    </Card.Text>
-                  <Button type="submit" className={styles.moderateButton} onClick={(e) => this.handleSubmit(e, article.title, "toAnalyse")}>Accept</Button>
-                  <Button type="submit" onClick={(e) => this.handleSubmit(e, article.title, "rejected")}>Decline</Button>
-                </Card.Body>
-              </Card>
-            </div>;
-          })}</ul>
-        </div>
+            return <div><ListGroup.Item action onClick={(e) => { this.handleOpen(e, article); this.updateDetails(article) }}>
+              Title: {article.title}
+            </ListGroup.Item>
+            </div>
+          })}
+        </ListGroup>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              {this.state.modelInformation.title}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Container>
+              <Row>
+                <Col><b>Author</b></Col>
+                <Col><b>Volume</b></Col>
+              </Row>
+              <Row>
+                <Col>{this.state.modelInformation.author}</Col>
+                <Col>{this.state.modelInformation.volume}</Col>
+              </Row>
+              <Row>
+                <Col><b>Page no.</b></Col>
+                <Col><b>No. of Pages</b></Col>
+              </Row>
+              <Row>
+                <Col>---</Col>
+                <Col>{this.state.modelInformation.numOfPages}</Col>
+              </Row>
+              <Row>
+                <Col><b>Year</b></Col>
+                <Col><b>Month</b></Col>
+              </Row>
+              <Row>
+                <Col>{this.state.modelInformation.year}</Col>
+                <Col>{this.state.modelInformation.month}</Col>
+              </Row>
+              <Row>
+                <Col><b>E-Print</b></Col>
+                <Col><b>E-Print Type</b></Col>
+              </Row>
+              <Row>
+                <Col>{this.state.modelInformation.ePrint}</Col>
+                <Col>{this.state.modelInformation.ePrintType}</Col>
+              </Row>
+              <Row>
+                <Col><b>E-Print Class</b></Col>
+                <Col><b>Annote</b></Col>
+              </Row>
+              <Row>
+                <Col>{this.state.modelInformation.ePrintClass}</Col>
+                <Col>{this.state.modelInformation.annote}</Col>
+              </Row>
+            </Container>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={(e) => this.handleSubmit(e, "toAnalyse")}>Accept</Button>
+            <Button onClick={(e) => this.handleSubmit(e, "rejected")}>Reject</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
