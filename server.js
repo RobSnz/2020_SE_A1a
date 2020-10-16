522627cb4cdd556de89455e4e80ae0e8584e0b78
Taylor Styles <taylor@aap.co.nz>
2020-10-16 19:48:25 +1300

    Tidying code

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const mongoose = require('mongoose');
 const compression = require('compression');
 const morgan = require('morgan');
 const path = require('path');
-
 const normalizePort = port => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5000);
-
 const app = express();
 const dev = app.get('env') !== 'production';
 
--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Results.css";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { Navbar } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
-import InputGroup from 'react-bootstrap/InputGroup';
-import SearchIcon from '@material-ui/icons/SearchSharp';
-import IconButton from '@material-ui/core/IconButton';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Navbar } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import InputGroup from "react-bootstrap/InputGroup";
+import SearchIcon from "@material-ui/icons/SearchSharp";
+import IconButton from "@material-ui/core/IconButton";
 import DatePicker from "react-datepicker";
 import axios from "axios";
 
@@ -15,16 +15,17 @@ class Results extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {                          //keeps track of state of users current search
-      search: '',
+    this.state = {
+      //keeps track of state of users current search
+      search: "",
       dateFrom: new Date("01/01/1990"),
       dateTo: new Date(),
       constraints: [],
       articleList: [],
-      data: ''
+      data: "",
     };
 
-    this.handleSubmit = this.handleSubmit.bind(this);                           //binders to handle data changes and present them
+    this.handleSubmit = this.handleSubmit.bind(this); //binders to handle data changes and present them
     this.handleFromDate = this.handleFromDate.bind(this);
     this.handleToDate = this.handleToDate.bind(this);
   }
@@ -34,84 +35,106 @@ class Results extends React.Component {
     this.getArticleResult();
   };
 
-  handleFromDate = dateFrom => {
+  handleFromDate = (dateFrom) => {
     this.setState({ dateFrom });
   };
 
-  handleToDate = dateTo => {
+  handleToDate = (dateTo) => {
     this.setState({ dateTo });
   };
 
-  handleSubmit(event) {                                               //once user clicks on search
+  handleSubmit(event) {
+    //once user clicks on search
     event.preventDefault();
   }
 
   getArticleResult = () => {
-    axios.post('/article/')
+    axios
+      .post("/article/")
       .then((response) => {
         const data = response.data;
         this.setState({ articleList: data });
       })
       .catch(() => {
-        alert('Error retrieving data');
+        alert("Error retrieving data");
       });
-  }
+  };
 
   render() {
-    
-
     return (
       <div>
         <Navbar bg="light" variant="light">
-              <Form>
-                  <Form.Row className="align-items-center">
-                      <Col sm={3} className="my-1" style={{ width: "1000px" }}>
-                          <InputGroup>
-                              <Form.Label htmlFor="inlineFormInputGroupUsername" srOnly>
-                                  Search
-                              </Form.Label>
-                              <Form.Control id="inlineFormInputName" placeholder="What are you looking for?" />
+          <Form>
+            <Form.Row className="align-items-center">
+              <Col sm={3} className="my-1" style={{ width: "1000px" }}>
+                <InputGroup>
+                  <Form.Label htmlFor="inlineFormInputGroupUsername" srOnly>
+                    Search
+                  </Form.Label>
+                  <Form.Control
+                    id="inlineFormInputName"
+                    placeholder="What are you looking for?"
+                  />
 
-                              <InputGroup.Append>
-                                  <IconButton aria-label="search" style={{ float: "right", margin: "-5px 0 0 0" }}>
-                                      <SearchIcon />
-                                  </IconButton>
-                              </InputGroup.Append>
-                          </InputGroup>
-                      </Col>
-                      <Col sm={3} className="my-1" style={{ margin: "0 -100px 0 0"}}>
-                        <DatePicker className="dateFontSize" selected={this.state.dateFrom} onChange={this.handleFromDate}/>
-                      </Col>
-                      <Col sm={3} className="my-1">
-                        <DatePicker className="dateFontSize" selected={this.state.dateTo} onChange={this.handleToDate} minDate={this.state.dateFrom} />
-                      </Col>
-                      <Col xs="auto" className="my-1">
-                          <Button type="submit" style={{ margin: "0 0 0 -60px"}}>Constraints</Button>
-                      </Col>
-                  </Form.Row>
-              </Form>
-          </Navbar>
-          {this.state.articleList.map(article => {
-             return <div> 
-                <form onSubmit={this.handleSubmit}>
-                  <div>
-                    <Card>
-                      <Card.Body>
-                        <Card.Title>Title: {article.title}</Card.Title>
-                        <Card.Text>
-                          Author: {article.author}<br/>
-                          Year: {article.year}<br/>
-                        </Card.Text>
-                        <Button variant="primary">Details</Button>
-                      </Card.Body>
-                    </Card>
-                  </div>
-                </form>
-              </div>;
-          })}
+                  <InputGroup.Append>
+                    <IconButton
+                      aria-label="search"
+                      style={{ float: "right", margin: "-5px 0 0 0" }}
+                    >
+                      <SearchIcon />
+                    </IconButton>
+                  </InputGroup.Append>
+                </InputGroup>
+              </Col>
+              <Col sm={3} className="my-1" style={{ margin: "0 -100px 0 0" }}>
+                <DatePicker
+                  className="dateFontSize"
+                  selected={this.state.dateFrom}
+                  onChange={this.handleFromDate}
+                />
+              </Col>
+              <Col sm={3} className="my-1">
+                <DatePicker
+                  className="dateFontSize"
+                  selected={this.state.dateTo}
+                  onChange={this.handleToDate}
+                  minDate={this.state.dateFrom}
+                />
+              </Col>
+              <Col xs="auto" className="my-1">
+                <Button type="submit" style={{ margin: "0 0 0 -60px" }}>
+                  Constraints
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Navbar>
+        {this.state.articleList.map((article) => {
+          return (
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <div>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>Title: {article.title}</Card.Title>
+                      <Card.Text>
+                        Author: {article.author}
+                        <br />
+                        Year: {article.year}
+                        <br />
+                      </Card.Text>
+                      <Button variant="primary">Details</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </form>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
 export default Results;
+
